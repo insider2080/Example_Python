3db2f0d42a5e80ae80893bcd07753ad27f81fa02
dkpark <dkpark@humaxdigital.com>
2015-06-13 13:14:45 +0900

    Initial commit

---
+++ Recursive.py
@@ -0,0 +1,36 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+                               Recursive Function
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def RecursiveFunction(i, count, index1, index2):
+    if (count == 0):
+        return;
+    else:
+        RecursiveFunction(i, count-1, index1, index2);
+        PrintFunction(i, count, index1, index2);
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+                                Print Function
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def PrintFunction(i, count, index1, index2):
+    if (i == 0):
+        print("\t'0%d': {"%count);
+    elif (i == 1):
+        if (count != index2):
+            print("\t\t'0%d-0%d': None,"%(index1, count));
+        else:
+            print("\t\t'0%d-0%d': {"%(index1, count));
+    elif (i == 2):
+        print("\t\t\t'0%d-0%d-0%d': None,"%(index1, index2, count));
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+                                 Main Function
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+print("tree = {");
+num_list = [1, 5, 4];
+
+for i in range(3):
+    RecursiveFunction(i, num_list[i], num_list[0], num_list[1]);
+
+print ("\t\t}");
+print ("\t}");
+print ("}");
--- SumTwoNumbers.py
+++ SumTwoNumbers.py
@@ -7,8 +7,8 @@ def SumFunction(input_list):
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
                             Calculation Function
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-def CalculateFuction(total):
-    if(total != 10):
+def CalculateFuction(sum):
+    if(sum != 10):
         print("You inputed wrong numbers. Please retry!");
     else:
         print("Congratulations!");
@@ -16,14 +16,14 @@ def CalculateFuction(total):
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
                                 Main Function
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-print("Input 2 numbers. If 2number's sum is 10, you can exit from infinite loop.");
+print("Input 2 numbers. If 2 number's sum is 10, you can exit from infinite loop.");
 #   Init variable
-total=0;
+sum=0;
 input_list = [0, 0];
 
 #   Input 2 numbers using 'while'
-while total != 10:
+while sum != 10:
     for i in range(2):
         input_list[i] = input();
-    total = SumFunction(input_list);
-    CalculateFuction(total);
+    sum = SumFunction(input_list);
+    CalculateFuction(sum);
